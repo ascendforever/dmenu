59a39f1e7fb56191a419fbbd76a667345c2383f5
ascendforever <georgeh124v2@gmail.com>
2026-02-14 11:12:20 -0500

    Change color scheme

--- config.def.h
+++ config.def.h
@@ -10,8 +10,8 @@ static const char *prompt      = NULL;      /* -p  option; prompt to the left of
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
 	[SchemeNorm] = { "#bbbbbb", "#222222" },
-	[SchemeSel] = { "#eeeeee", "#005577" },
-	[SchemeOut] = { "#000000", "#00ffff" },
+	[SchemeSel]  = { "#ffffff", "#70101d" },
+	[SchemeOut]  = { "#c93a3a", "#ffffff" },
 	[SchemeCursor] = { "#222222", "#bbbbbb"},
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
@@ -34,3 +34,4 @@ static Key quit_keys[] = {
 	/* keysym	modifier */
 	{ XK_q,		0 }
 };
+
