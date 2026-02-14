96e156cc773da35a2f3b6368a3d17d8327a6283c
ascendforever <georgeh124v2@gmail.com>
2026-02-14 11:31:26 -0500

    Vi mode defaults to normal mode

--- config.def.h
+++ config.def.h
@@ -28,7 +28,7 @@ static const char worddelimiters[] = " ";
  * accessed with the global_esc keysym + mod mask
  */
 static unsigned int vi_mode = 1;
-static unsigned int start_mode = 0;			/* mode to use when -vi is passed. 0 = insert mode, 1 = normal mode */
+static unsigned int start_mode = 1;			/* mode to use when -vi is passed. 0 = insert mode, 1 = normal mode */
 static Key global_esc = { XK_n, Mod1Mask };	/* escape key when vi mode is not enabled explicitly */
 static Key quit_keys[] = {
 	/* keysym	modifier */
